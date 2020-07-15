cd5043320d17b01942486e5c6e4df877e28cd329
Alfred <appiagyeialfred815@gmail.com>
2020-07-15 14:32:25 +0000

    router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
 import './App.css';
 
-function App() {
+import {
+
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+
+} from "react-router-dom";
+
+
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      
+      <div>
+      
+        
+               <ul  style={{display:"flex",}}>
+            
+                <li>
+                <Link to="/"><b>Home</b> </Link>
+                  </li>
+                <li>
+                <Link to="/About"><b>About</b></Link>
+                  </li>
+                <li>
+                <Link to="/Services"><b>Services</b> </Link>
+                  </li>
+                <li>
+                <Link to="/Portfolio"><b>Portfolio</b></Link>
+                  </li>
+                  <li>
+                <Link to="/Contacts"><b>Contacts</b></Link>
+                  </li>
+              </ul>
+           
+
+            <Switch>
+              <Route path="/" exact component={Home}  />
+              <Route path="/about" component={About} />
+              <Route path="/services" component={Services} />
+              <Route path="/contacts" component={Contacts} />
+              <Route path="/portfolio" component={Portfolio} />
+             </Switch>
+      
+    
+            </div>
+      </Router>
   );
 }
 
-export default App;
+
+
